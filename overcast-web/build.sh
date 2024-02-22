7482b3681b254a361ccdac2380da3bed9105be88
= <=>
2024-02-22 07:44:00 +0100

    added building system for web

---
+++ overcast-web/build.sh
@@ -0,0 +1,8 @@
+# build the overcast client
+cd ../overcast-client
+cargo build --release --target wasm32-unknown-unknown
+# go back and copy the wasm
+cd ../overcast-web
+cp ../overcast-client/target/wasm32-unknown-unknown/release/overcast-client.wasm ./wasm/
+# generate the js to bind wasm
+wasm-bindgen --no-typescript --target web --out-dir ./js/ --out-name "overcast" ./wasm/overcast-client.wasm
---
+++ overcast-web/run.sh
@@ -0,0 +1,2 @@
+# serve a web server right here, the index.html will do the work
+python3 -m http.server 8080
